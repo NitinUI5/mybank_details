927aa3509b5827a99ddcdf1976eee0be075bd41b
Nitin Meena <nitin11meena@gmail.com>
2024-01-26 11:29:22 +0000

    updates ragment dialog

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -10,6 +10,29 @@ sap.ui.define([
         return Controller.extend("com.nm100.mybankdetails.controller.App", {
             onInit: function () {
 
+            },
+
+            onOpenBankDetails: function () {
+
+                //create dialog lazily
+
+                if (!this.moreBankDetails) {
+                    this.moreBankDetails = this.loadFragment(
+                        { name: "com.nm100.mybankdetails.view.fragments.MoreDetails" }
+
+                    );
+                }
+
+                this.moreBankDetails.then(
+                    function (oDialog) {
+                        oDialog.open();
+
+                    }
+                );
+            },
+
+            onCloseBankDetails: function () {
+                this.byId("moreBankDetails").close();
             }
         });
     });
